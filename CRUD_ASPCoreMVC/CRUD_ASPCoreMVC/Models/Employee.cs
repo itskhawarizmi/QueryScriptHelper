06a2966bf4a6a46431cacad485fc08f704468cf3
itskhawarizmi <itskhawarizmi@gmail.com>
2019-09-18 23:10:02 +0700

    Add new files

---
+++ CRUD_ASPCoreMVC/CRUD_ASPCoreMVC/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_ASPCoreMVC.Models
+{
+    public class Employee
+    {
+        [Required]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Department { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public string ContactNo { get; set; }
+        
+        public DateTime DOB { get; set; }
+    }
+}
